1d1dbf33c1bb7c9ade00cbca001de4832d3c4d08
InhaKr <inkr3891@gmail.com>
2023-01-19 21:34:22 +0100

    js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,11 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function (data) {
+const play = function (data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(play, 1000));
 
 const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
