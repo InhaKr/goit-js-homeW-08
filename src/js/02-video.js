04c5881257030c26eab8ce3255bf87a9ed179746
InhaKr <inkr3891@gmail.com>
2023-01-20 16:26:57 +0100

    2 exercise.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -14,19 +14,4 @@ const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
 player.setCurrentTime(currentTime).then(function (seconds) {
   // seconds = the actual time that the player seeked to
-}).catch(function (error) {
-  switch (error.name) {
-    case 'RangeError':
-      // the time was less than 0 or greater than the video’s duration
-      break;
-    default:
-      // some other error occurred
-      break;
-  }
-});
-
-player.setColor('#45a247').then(function (color) {
-  // the color that was set
-}).catch(function (error) {
-  // an error occurred setting the color
-});
+})
